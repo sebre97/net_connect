b1ba92683c979171e4822fd7669a12d4963708e9
Gandalf3141 <47748007+Gandalf3141@users.noreply.github.com>
2022-02-08 22:42:35 +0100

    Add files via upload

--- importTEST.py
+++ importTEST.py
@@ -2,7 +2,7 @@ import pandas as pd
 import geopandas as geo
 import matplotlib.pyplot as plt
 
-
+#Ich habe einen Kommentar hinzugefügt um push zu test lg. bussi
 
 countries_gdf = geo.read_file("Daten/raster_austria.gpkg", rows=10)
 
