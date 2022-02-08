aad445d28e7b0c38c955311c12596708b12ce3f2
Gandalf3141 <47748007+Gandalf3141@users.noreply.github.com>
2022-02-08 22:20:33 +0100

    Ein Test um die Daten zu importieren

---
+++ importTEST.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import geopandas as geo
+import matplotlib.pyplot as plt
+
+
+
+countries_gdf = geo.read_file("Daten/raster_austria.gpkg", rows=10)
+
+print(countries_gdf.geometry)
+print(countries_gdf.geometry.name)
+
+#countries_gdf.plot()
+countries_gdf.plot()
+plt.show()
